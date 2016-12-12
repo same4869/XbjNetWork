7d0b51c75601615929da0eeb8053128b2f3a1fe5
xwang <xunwang@wenba100.com>
2016-12-12 14:50:41 +0800

    download

--- app/src/main/java/com/xbjnw/wangxun/xbjnetwork/XBJNWMainActivity.java
+++ app/src/main/java/com/xbjnw/wangxun/xbjnetwork/XBJNWMainActivity.java
@@ -1,13 +1,19 @@
 package com.xbjnw.wangxun.xbjnetwork;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.xbjnw.wangxun.xbjnetwork.web.WenbaImageLoader;
 import com.xbjnw.wangxun.xbjnetwork.web.WenbaRequest;
+import com.xbjnw.wangxun.xbjnetwork.webbase.WenbaDownLoader;
 import com.xbjnw.wangxun.xbjnetwork.webbase.WenbaWebLoader;
+import com.xbjnw.wangxun.xbjnetwork.webbase.core.WenbaDownloadListener;
 import com.xbjnw.wangxun.xbjnetwork.webbase.core.WenbaResponse;
 import com.yolanda.nohttp.RequestMethod;
 
@@ -15,6 +21,9 @@ import java.util.HashMap;
 
 public class XBJNWMainActivity extends AppCompatActivity {
     private ImageView imageView;
+    private Button startBtn, stopBtn, contiuneBtn;
+    private ProgressBar progressBar;
+    private int taskId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +33,63 @@ public class XBJNWMainActivity extends AppCompatActivity {
         requestGuidePic();
 
         imageView = (ImageView) findViewById(R.id.test_im);
-        WenbaImageLoader.getInstance(getApplicationContext()).displayImage("http://ww3.sinaimg.cn/mw690/5f623dcegw1fakxd784zjj20zk0qo45h.jpg", imageView);
+        WenbaImageLoader.getInstance(getApplicationContext()).displayImage("http://ww3.sinaimg" +
+                ".cn/mw690/5f623dcegw1fakxd784zjj20zk0qo45h.jpg", imageView);
+        startBtn = (Button) findViewById(R.id.start_download);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                download("http://www.bmob.cn/sdk/Bmob_AndroidSDK_V3.5.2_1027.zip");
+            }
+        });
+
+        stopBtn = (Button) findViewById(R.id.pause_download);
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WenbaDownLoader.getDownloadRequests().get(taskId).cancel();
+            }
+        });
+    }
+
+    /**
+     * 下载
+     *
+     * @param appUrl
+     */
+    private void download(String appUrl) {
+        // 下载文件
+        String downloadPath = Environment.getExternalStorageDirectory().getPath() + "/123.a";
+        Log.d("kkkkkkkk", "downloadPath --> " + downloadPath);
+        taskId = WenbaDownLoader.download(appUrl, downloadPath, new WenbaDownloadListener() {
+            @Override
+            public void onDownloadError(String msg) {
+                Log.d("kkkkkkkk", "onDownloadError msg --> " + msg);
+            }
+
+            @Override
+            public void onStart() {
+                Log.d("kkkkkkkk", "onStart");
+            }
+
+            @Override
+            public void onProgress(int progress, long fileCount) {
+                Log.d("kkkkkkkk", "progress  --> " + progress);
+                progressBar.setProgress(progress);
+            }
+
+            @Override
+            public void onFinish(String filePath) {
+                Log.d("kkkkkkkk", "onFinish");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("kkkkkkkk", "onCancel");
+            }
+        });
     }
 
     private void requestGuidePic() {
@@ -34,7 +99,8 @@ public class XBJNWMainActivity extends AppCompatActivity {
         params.put("word", "盗墓笔记");
         params.put("page", "1");
         params.put("src", "人民日报");
-        final WenbaRequest request = new WenbaRequest("http://apis.baidu.com/txapi/weixin/wxhot", RequestMethod.GET, params, new
+        final WenbaRequest request = new WenbaRequest("http://apis.baidu.com/txapi/weixin/wxhot", RequestMethod.GET,
+                params, new
                 WenbaResponse<ResponseBean>() {
 
                     @Override
@@ -43,7 +109,8 @@ public class XBJNWMainActivity extends AppCompatActivity {
                             Log.d("kkkkkkkk", "response --> " + response.toString());
                             if (response.getNewslist() != null) {
                                 for (int i = 0; i < response.getNewslist().size(); i++) {
-                                    Log.d("kkkkkkkk", " response.getNewslist().get(i).getTitle() --> " + response.getNewslist().get(i).getTitle());
+                                    Log.d("kkkkkkkk", " response.getNewslist().get(i).getTitle() --> " + response
+                                            .getNewslist().get(i).getTitle());
                                 }
                             }
                         }
--- app/src/main/java/com/xbjnw/wangxun/xbjnetwork/webbase/WenbaDownLoader.java
+++ app/src/main/java/com/xbjnw/wangxun/xbjnetwork/webbase/WenbaDownLoader.java
@@ -1,5 +1,7 @@
 package com.xbjnw.wangxun.xbjnetwork.webbase;
 
+import android.util.Log;
+
 import com.xbjnw.wangxun.xbjnetwork.webbase.core.HttpCancel;
 import com.xbjnw.wangxun.xbjnetwork.webbase.core.WenbaDownloadListener;
 import com.yolanda.nohttp.NoHttp;
@@ -8,6 +10,8 @@ import com.yolanda.nohttp.download.DownloadQueue;
 import com.yolanda.nohttp.download.DownloadRequest;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Lijj on 16/9/22.
@@ -19,11 +23,12 @@ public class WenbaDownLoader {
      * 下载队列.
      */
     private static DownloadQueue mDownloadQueue;
+    private static List<DownloadRequest> downloadRequests = new ArrayList<>();
 
     private static DownloadQueue getDownloadQueue() {
-        if(mDownloadQueue == null){
+        if (mDownloadQueue == null) {
             synchronized (WenbaDownLoader.class) {
-                if(mDownloadQueue == null) {
+                if (mDownloadQueue == null) {
                     mDownloadQueue = NoHttp.newDownloadQueue(2);
                 }
             }
@@ -33,48 +38,55 @@ public class WenbaDownLoader {
     }
 
     /**
-     *
-     * @param url 下载地址。
+     * @param url      下载地址。
      * @param filePath 保存的文件路径
      * @param listener
      */
-    public static HttpCancel download(String url, String filePath, WenbaDownloadListener listener) {
+    public static int download(String url, String filePath, WenbaDownloadListener listener) {
         return download(url, filePath, true, listener);
     }
 
     /**
-     *
-     * @param url 下载地址。
+     * @param url      下载地址。
      * @param filePath 保存的文件路径
-     * @param isRange 是否断点续传下载
+     * @param isRange  是否断点续传下载
      * @param listener
      */
-    public static HttpCancel download(String url, String filePath, boolean isRange, WenbaDownloadListener listener) {
+    public static int download(String url, String filePath, boolean isRange, WenbaDownloadListener listener) {
         File file = new File(filePath);
+        Log.d("kkkkkkkk", "file.getName() --> " + file.getName());
         DownloadRequest request = NoHttp.createDownloadRequest(url, RequestMethod.GET, file.getParent(), file.getName
                 (), isRange, true);
 
-        HttpCancel cancle = new HttpCancel();
-        request.setCancelSign(cancle);
+//        HttpCancel cancle = new HttpCancel();
+//        request.setCancelSign(cancle);
 
-        if(listener != null) {
+        downloadRequests.add(request);
+
+        if (listener != null) {
             listener.setUrl(url);
         }
 
         getDownloadQueue().add(0, request, listener);
 
-        return cancle;
+//        return cancle;
+        int taskId = downloadRequests.size() - 1;
+        return taskId;
     }
 
-    public static void startAll(){
+    public static List<DownloadRequest> getDownloadRequests(){
+        return downloadRequests;
+    }
+
+    public static void startAll() {
         getDownloadQueue().start();
     }
 
-    public static void cancelAll(){
+    public static void cancelAll() {
         getDownloadQueue().cancelAll();
     }
 
-    public static void cancelBySign(HttpCancel sign){
+    public static void cancelBySign(HttpCancel sign) {
         getDownloadQueue().cancelBySign(sign);
     }
 
